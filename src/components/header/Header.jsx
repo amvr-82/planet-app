cbfc0243347b31268fac9a308db50f141afdecf2
amvr-82 <amirhossein.develope@gmail.com>
2026-02-11 00:15:41 +0330

    add blur effect

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,20 @@
 export default function Header() {
   return (
-    <header className="bg-red-800 fixed top-0 w-full h-20 z-5000 flex items-center px-6">
-      <div className=" flex flex-row items-center gap-1 ml-0">
-        <img src="/assets/icon/logo.png" alt="" width={40} height={40} />
-        <span className="text-[28px]">Planto.</span>
+    <div className="fixed top-0 w-full h-20 z-50">
+      {/* خود هدر*/}
+      <div className="absolute inset-0 flex items-center px-6 z-10">
+        <img src="/assets\icon\logo.png" alt="" width={40} height={40} />
       </div>
-    </header>
+
+      {/* اینجا pointer-event  مهمههههه*/}
+      <div
+        className="bg-neutral-950/45 absolute inset-0 backdrop-blur-[10px] pointer-events-none"
+        style={{
+          maskImage: "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))",
+          WebkitMaskImage:
+            "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))",
+        }}
+      ></div>
+    </div>
   );
 }
