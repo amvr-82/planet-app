3c7a3f9f00bd7bd5db947bf19a27388cd6f9a195
amvr-82 <amirhossein.develope@gmail.com>
2026-02-10 20:19:04 +0330

    add hrader

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 w-full backdrop-blur-md bg-white/30 shadow-md z-50">
+      khhglkhjbkb
+    </header>
+  );
+}
