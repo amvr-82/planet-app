46842ec2be9082088f14b3e289a0ee2cff436423
amvr-82 <amirhossein.develope@gmail.com>
2026-02-11 13:50:33 +0330

    cover image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,12 @@ export default function Header() {
     <div className="fixed items-center top-0 w-full h-20 z-50">
       {/* خود هدر*/}
       <div className="absolute inset-0 flex items-center  align-middle  px-6 z-10">
+        {/* دیو لوگوعه */}
         <div className=" flex gap-2 items-center mt-6 px-2">
           <img src="/assets\icon\logo.png" alt="" width={40} height={40} />
           <span className="text-[28px] text-white ">Planto</span>
         </div>
+        {/* دیو منوعه */}
         <div className="">
           <nav className=" flex absolute top-10 left-1/2 transform -translate-x-1/2 gap-10 font-linter text-gray-200 text-[24px] ">
             <a className="hover:opacity-70 transition-opacity" href="home">
@@ -23,6 +25,7 @@ export default function Header() {
             </a>
           </nav>
         </div>
+        {/* دیو ایکون های سمت راسته */}
         <div className=" absolute flex gap-12 right-0 px-15 top-10 ">
           <img
             className="cursor-pointer hover:opacity-70 transition-opacity"
@@ -48,9 +51,9 @@ export default function Header() {
         </div>
       </div>
 
-      {/* اینجا pointer-event  مهمههههه*/}
+      {/* دیوی که بلر کرده اینه و اینجا {pointer-event} مهمه*/}
       <div
-        className="bg-neutral-950/45 absolute inset-0 backdrop-blur-[10px] pointer-events-none"
+        className="bg-neutral-950/45 absolute inset-0 backdrop-blur-[100px] pointer-events-none"
         style={{
           maskImage: "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))",
           WebkitMaskImage:
