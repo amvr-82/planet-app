7e0c0fb74c51fe8b83bede31df79ddb2dff17a77
amvr-82 <amirhossein.develope@gmail.com>
2026-02-11 17:55:49 +0330

    add image

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,10 +3,12 @@ import Card from "../components/component/card";
 
 export default function Home() {
   return (
-    <div className="pt-100">
+    <div className="pt-80">
       <div>
         <Header />
-        <Card />
+        <div className=" absolute right-0">
+          <Card />
+        </div>
       </div>
     </div>
   );
--- src/components/component/Card-hero.jsx
+++ src/components/component/Card-hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Card() {
-  return <div className="card"></div>;
+  return <div className="card "></div>;
 }
 
 export default Card;
