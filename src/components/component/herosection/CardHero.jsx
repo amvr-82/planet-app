e347a689244a4b114b0a7697ee9c2a97ba7f39db
amvr-82 <amirhossein.develope@gmail.com>
2026-02-11 20:44:22 +0330

    rename aks

--- src/components/component/herosection/CardHero.jsx
+++ src/components/component/herosection/CardHero.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 function CardHero() {
   return (
-    <div className="relative  w-125 h-125 flex items-center justify-center">
-      {/* عکس گل */}
-      <div>
+    <div className="relative w-125 h-125 flex items-center justify-center">
+      {/* عکس گل*/}
+      <div className="absolute left-1/2 -top-35 -translate-x-1/2 z-60 pointer-events-none">
         <img
-          className="absolute left-1/2 -top-45 
-         -translate-x-1/2 
-          scale-100 z-50 pointer-events-none"
+          className=""
           src="./assets/image/planet1.png"
           alt="Planet"
           width={410}
@@ -16,16 +14,20 @@ function CardHero() {
         />
       </div>
 
-      {/* کارت اصلی */}
-      <div className=" card relative z-0   flex flex-col justify-end ">
-        <div className="  flex flex-col gap-2 my-15 mx-10">
+      {/* */}
+      <div className="card relative z-50 flex flex-col justify-end">
+        <div className="flex flex-col gap-2 my-15 mx-10">
           <span className="text-white/75 text-[24px] font-medium">
             Trendy House Plant
           </span>
           <span className="text-white/75 text-[48px] font-light">
             Calathea plant
           </span>
-          <button className="cursor-pointer text-white/75 text-[24px] border-2 border-white/50 rounded-[15px] w-40 h-12 ">
+          {/* با استایل های تست شده از بالا */}
+          <button
+            className="cursor-pointer text-white/75 text-[24px] border-2 border-white/50 rounded-[15px] w-40 h-12 
+                       hover:bg-white/20 active:bg-white/40 transition-colors duration-150 z-99"
+          >
             Buy Now
           </button>
         </div>
