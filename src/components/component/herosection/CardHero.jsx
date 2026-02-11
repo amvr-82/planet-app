04cb7f361445c8cd0af460b8047e248e4b5de381
amvr-82 <amirhossein.develope@gmail.com>
2026-02-12 00:58:01 +0330

    add detail

--- src/components/component/herosection/CardHero.jsx
+++ src/components/component/herosection/CardHero.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function CardHero() {
   return (
-    <div className="relative w-125 h-125 flex items-center justify-center">
+    <div className=" relative w-125 h-125 flex items-center justify-center">
       {/* عکس گل*/}
-      <div className="absolute left-1/2 -top-7 -translate-x-1/2 z-60 pointer-events-none">
+      <div className="absolute left-1/2 -top-3 -translate-x-1/2 z-60 pointer-events-none">
         <img
-          className="scale-120"
+          className="scale-110"
           src="./assets/image/plant1.png"
           alt="Planet"
           width={500}
@@ -15,14 +15,14 @@ function CardHero() {
       </div>
 
       {/* */}
-      <div className="card relative z-50 flex flex-col justify-end">
-        <div className="flex flex-col gap-2 my-15 mx-10">
+      <div className=" card relative z-50 flex flex-col justify-end">
+        <div className=" flex flex-col gap-2 my-9 p-2 mx-10">
           <span className="text-white/75 text-[17px] font-light">
             Trendy House Plant
           </span>
           <span className="text-white/75 text-[24px] ">Calathea plant</span>
           <button
-            className="cursor-pointer text-white/75 text-[20px] border-2 border-white/50 rounded-2xl w-30 h-12 
+            className="cursor-pointer font-Inter text-white/75 text-[20px] border-2 border-white/50 rounded-xl w-40 h-12
                        hover:bg-white/20 active:bg-white/40 transition-colors duration-150 z-99"
           >
             Buy Now
--- src/components/component/herosection/Herosection.jsx
+++ src/components/component/herosection/Herosection.jsx
@@ -3,8 +3,8 @@ import CardHero from "./CardHero";
 
 function Herosection() {
   return (
-    <div className="pt-90">
-      <div className="absolute right-14">
+    <div className=" pt-90">
+      <div className=" absolute right-14">
         <CardHero />
       </div>
     </div>
