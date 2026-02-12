93418d0768170c66c190af748a957879c039e729
amvr-82 <amirhossein.develope@gmail.com>
2026-02-12 23:09:16 +0330

    add plant2 + div second

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -8,7 +8,14 @@ export default function Home() {
       <Header />
       <Herosection />
       <Call />
-      <div className="card1 rounded-[60px] w-[90%] h-80 ml-auto mr-auto mt-10"></div>
+      {/* دیو کرات بزرگ زیر هدینگ */}
+      <div className="flex card1 rounded-[90px] w-[90%] h-80 ml-auto mr-auto mt-50">
+        {/* دیو عکس */}
+        <div className="z-50 -mt-45">
+          <img src="./public\assets\image\plant2.png" alt="" width={500} />
+        </div>
+        <div className="bg-yellow-500">ddd</div>
+      </div>
     </div>
   );
 }
