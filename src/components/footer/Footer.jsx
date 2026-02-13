053a6d524f58076c8b3a51afe83a56c8f3b81938
amvr-82 <amirhossein.develope@gmail.com>
2026-02-13 23:36:22 +0330

    add color to footer divs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <div className="bg-[rgb(34,44,29)]  h-100 w-full flex rounded-[30px_30px_0px_0px] shadow-[2px -20px 2px rgb(59,77,50)]  mt-20 z-999">
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="bg-red-500"></div>
+      <div className="bg-yellow-500"></div>
+      <div className="bg-green-500"></div>
     </div>
   );
 }
