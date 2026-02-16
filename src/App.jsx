b0e2178de785cfad41cc83e74934dd6cc6781221
amvr-82 <amirhossein.develope@gmail.com>
2026-02-17 00:05:44 +0330

    add router in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import Home from "./Home/Home";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layout.jsx";
+import Home from "./pages/Home.jsx";
+import PlantType from "./pages/palntType";
 
 function App() {
   return (
-    <div className="back-img">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="PalntType" element={<PlantType />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
