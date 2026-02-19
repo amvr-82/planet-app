f8def0c07379ea54ad1630bd93fc3960186c152e
amvr-82 <amirhossein.develope@gmail.com>
2026-02-20 00:21:33 +0330

    sas

--- src/components/component/others/Call.jsx
+++ src/components/component/others/Call.jsx
@@ -4,7 +4,7 @@ import calling from "/public/assets/icon/call2.json";
 function Call() {
   return (
     <div className="fixed bottom-0 right-0 sm:right-4 z-50 cursor-pointer group">
-      <div className="absolute inset-0 rounded-full bg-white blur-[30px] opacity-40 group-hover:bg-green-700/70 transition"></div>
+      <div className="absolute inset-0 rounded-full bg-white blur-[25px] blur-3xl opacity-40 group-hover:bg-green-700/70 transition"></div>
 
       <Lottie
         animationData={calling}
