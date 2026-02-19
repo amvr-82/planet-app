b8f2e639da52b23632b8277ccac121d12d0ca66c
amvr-82 <amirhossein.develope@gmail.com>
2026-02-19 23:58:06 +0330

    add in home page only

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,14 @@ import Home from "./pages/Home.jsx";
 
 function App() {
   return (
-    <div className="back-img">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            {/* <Route path="PalntType" element={<PlantType />} /> */}
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="PalntType" element={<PlantType />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
