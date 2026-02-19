8ff46be8155c94341c234c02256a916ff4dccef3
amvr-82 <amirhossein.develope@gmail.com>
2026-02-19 13:36:10 +0330

    delete bulid and add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./layout.jsx";
+import Layout from "./Layout.jsx";
 import Home from "./pages/Home.jsx";
 // import PlantType from "./pages/PlantType.jsx";
 
