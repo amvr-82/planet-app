f7e7a23b9aeffa6bd5c3424d45ea76d41ec02783
amvr-82 <amirhossein.develope@gmail.com>
2026-02-20 21:47:06 +0330

    add data & structure

--- src/pages/PlantType.jsx
+++ src/pages/PlantType.jsx
@@ -1,9 +1,59 @@
 import React from "react";
-
+import { useState } from "react";
 function PlantType() {
+  const [index, setIndex] = useState(0);
+  // const next = () => setIndex((prev) => (prev + 1) % slides.length);
+  // const prev = () =>
+
+  const slides1 = [
+    {
+      title: "We Have Small And Best O1 Plants Collection’s",
+      subtitle: "Trendy House Plant",
+      image: "/assets/image/plant1.png",
+    },
+    {
+      title: "We Have Small And Best O2 Plants Collection’s",
+      subtitle: "Fresh Green Plant",
+      image: "/assets/image/plant2.png",
+    },
+    {
+      title: "We Have Small And Best O3 Plants Collection’s",
+      subtitle: "Modern Home Plant",
+      image: "/assets/image/plant3.png",
+    },
+  ];
   return (
     <div className="bg-[#1B2316] h-600">
-      <div className="card absolute"></div>
+      <div className="card1 absolute  top-20 mx-20 h-200 rounded-[30px] overflow-hidden">
+        {/* اسلاید فقط برای متن و دکمه */}
+        <div
+          className=" absolute inset-0 flex transition-transform ease-in-out duration-1000"
+          style={{ transform: `translateX(-${index * 100}%)` }}
+        >
+          {slides1.map((item) => (
+            <div
+              key={item.title}
+              className="  w-full shrink-0 flex flex-col justify-end"
+            >
+              <div>
+                <img src={item.image} alt="" />
+              </div>
+              <div className="  bg-yellow-500 flex flex-col gap-2 my-9 p-2 mx-10">
+                <span className="text-white/75 text-[17px] font-light">
+                  {item.subtitle}
+                </span>
+                <span className="text-white/75 text-[24px]">{item.title}</span>
+                <button
+                  type="button"
+                  className="cursor-pointer text-white/75 text-[18px] sm:text-[20px] border-2 border-white/50 rounded-xl w-40 h-12 hover:bg-white/20 active:bg-white/40 transition-colors duration-150"
+                >
+                  Buy Now
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
